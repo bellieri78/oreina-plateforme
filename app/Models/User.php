204586f92c65fe3087e34dabb37325e4ab7eba4f
David Demerges <d.demerges@cenlorraine.fr>
2026-04-15 21:03:15 +0200

    feat(auth): User implements MustVerifyEmail + grandfather existing users

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Cache;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
