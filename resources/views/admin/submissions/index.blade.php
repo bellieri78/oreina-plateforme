0a7a68390e45e5828e080c08a02b8e5ffd935e31
David Demerges <d.demerges@cenlorraine.fr>
2026-04-18 15:06:08 +0200

    feat(admin): hide submission create button without editorial capability

--- resources/views/admin/submissions/index.blade.php
+++ resources/views/admin/submissions/index.blade.php
@@ -21,12 +21,14 @@
                 </svg>
                 Exporter
             </a>
+            @can('create-submission-for-author')
             <a href="{{ route('admin.submissions.create') }}" class="btn btn-primary">
                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" width="16" height="16">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
                 </svg>
                 Nouvelle soumission
             </a>
+            @endcan
         </div>
     </div>
 
