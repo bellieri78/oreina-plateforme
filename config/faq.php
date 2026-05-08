e1644326cae2cdd6c2f359026d54870b9d22224a
David Demerges <d.demerges@cenlorraine.fr>
2026-05-08 14:43:46 +0200

    feat(faq): config sections (slug/label/color/icon)

---
+++ config/faq.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'sections' => [
+        ['slug' => 'association', 'label' => "1. L'association",                          'title' => 'Qui sommes-nous, comment ça marche',  'color' => 'sage',  'icon' => 'landmark'],
+        ['slug' => 'adherer',     'label' => '2. Adhérer & contribuer',                   'title' => 'Devenir membre, bénévole, donateur',  'color' => 'coral', 'icon' => 'heart-plus'],
+        ['slug' => 'papillons',   'label' => '3. Les papillons',                          'title' => 'Connaissances générales',             'color' => 'gold',  'icon' => 'bug'],
+        ['slug' => 'identifier',  'label' => '4. Identifier, observer, photographier',   'title' => 'La pratique de terrain',              'color' => 'blue',  'icon' => 'binoculars'],
+        ['slug' => 'donnees',     'label' => '5. Données & Artemisiae',                   'title' => 'Saisir, qualifier, partager',         'color' => 'sage',  'icon' => 'database'],
+        ['slug' => 'ethique',     'label' => '6. Science, éthique et conservation',       'title' => 'Les questions de fond',               'color' => 'coral', 'icon' => 'scale'],
+    ],
+];
